19711d783c8c598e72abd0282bce8207be2fd910
Robert9547 <00857002@email.ntou.edu.tw>
2024-09-18 03:37:23 +0800

    feat:py_test

---
+++ test_example.py
@@ -0,0 +1,15 @@
+def test_addition():
+    assert 1 + 1 == 2
+
+def test_subtraction():
+    assert 5 - 2 == 3
+
+def test_multiplication():
+    assert 2 * 3 == 6
+
+def test_division():
+    assert 10 / 2 == 5
+
+def test_hello():
+    message = "hello"
+    assert message == "hello"
